d0919954d3ba4d4d90f96e617a4131fb00ae3068
Xavier <Xavierlee.dev@gmail.com>
2026-02-02 20:52:04 -0700

    Complete Monday Challenge: Identity variables and f-strings

---
+++ 01_variables.py
@@ -0,0 +1,9 @@
+# My Idebtity Variables
+first_name = "Lee"
+goal_income = 10000 # Put your actual number here
+target_niche = "Automation Specialist"
+
+# The F-String Exercise
+message = f"My name is {first_name}. I am an {target_niche} developer, and my goal is to earn ${goal_income} per month to provide for my family."
+
+print(message) # <--- Without this line, the screen stays blank!
